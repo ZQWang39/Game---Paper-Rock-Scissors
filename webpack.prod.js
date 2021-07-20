8adcea08d51e37aacaf64736d0f05b716d80d3a2
Ziqi Wang <ziqi.wang39@gmail.com>
2021-07-20 15:28:17 +1000

    Update webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -47,7 +47,7 @@ module.exports = {
         cleanStaleWebpackAssets: true,
         protectWebpackAssets: false
     }),
-    new WorkboxPlugin.GenerateSW(),
+    // new WorkboxPlugin.GenerateSW(),
     new MiniCssExtractPlugin({ 
       filename: "[name].css",
   }),
